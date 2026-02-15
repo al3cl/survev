e1b9559ecda27991b61f8ac7bcbed144114c9b41
AL3CL <63611672+AL3CL@users.noreply.github.com>
2026-02-15 00:23:23 -0500

    Create extra perk UI elements

--- client/src/ui/ui2.ts
+++ client/src/ui/ui2.ts
@@ -35,7 +35,7 @@ import type { Localization } from "./localization";
 
 const maxKillFeedLines = 6;
 const touchHoldDuration = 0.75 * 1000;
-const perkUiCount = 4;
+const perkUiCount = 24;
 
 enum InteractionType {
     None,
