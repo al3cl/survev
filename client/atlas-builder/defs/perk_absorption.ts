e2d7b7eecbe8ea9c8630a20d7fc93eb8b6f95376
AL3CL <63611672+AL3CL@users.noreply.github.com>
2026-02-19 00:25:19 -0500

    Add custom perk box texture

--- client/atlas-builder/defs/perk_absorption.ts
+++ client/atlas-builder/defs/perk_absorption.ts
@@ -3,6 +3,6 @@ import type { AtlasDef } from "../atlasDefs";
 export const PerkAbsorptionAtlas: AtlasDef = {
     compress: true,
     images: [
-        "map/map-crate-21.svg",
+        "ggbruh/map-crate-ggbruh-perk.svg",
     ],
 };
--- shared/defs/mapObjectDefs.ts
+++ shared/defs/mapObjectDefs.ts
@@ -30750,9 +30750,10 @@ export const MapObjectDefs: Record<string, MapObjectDef> = {
         loot: [
             tierLoot("tier_perk_absorption_perks", 1, 1),
         ],
-        map: { display: true, color: 0x496f, scale: 0.875 },
+        // map: { display: true, color: 0x49006f, scale: 0.875 },
+        map: { display: true, color: 0x7100ad, scale: 0.875 },
         terrain: { grass: true, beach: false },
-        img: { sprite: "map-crate-21.img" },
+        img: { sprite: "map-crate-ggbruh-perk.img" },
         sound: { explode: "crate_break_01" },
     }),
 };
