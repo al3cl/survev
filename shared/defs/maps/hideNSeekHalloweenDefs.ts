db9a8afad82294353b92373617ea937b9c778caf
alec <63611672+AL3CL@users.noreply.github.com>
2026-03-02 20:33:32 -0500

    Remove Halloween perks from HnSH map
    the perk pumpkins are still there for decoration

--- shared/defs/maps/hideNSeekHalloweenDefs.ts
+++ shared/defs/maps/hideNSeekHalloweenDefs.ts
@@ -16,6 +16,23 @@ const mapDef: PartialMapDef = {
     gameMode: {
         hideNSeek: true,
     },
+    lootTable: {
+        // tier_pumpkin_perks: [{ name: "halloween_mystery", count: 1, weight: 1 }],
+        tier_pumpkin_perks: [{ name: "", count: 1, weight: 1 }],
+        // tier_halloween_mystery_perks: [
+        //     { name: "trick_nothing", count: 1, weight: 1 },
+        //     { name: "trick_size", count: 1, weight: 1 },
+        //     { name: "trick_m9", count: 1, weight: 1 },
+        //     { name: "trick_chatty", count: 1, weight: 1 },
+        //     { name: "trick_drain", count: 1, weight: 1 },
+        //
+        //     { name: "treat_9mm", count: 1, weight: 1 },
+        //     { name: "treat_12g", count: 1, weight: 1 },
+        //     { name: "treat_556", count: 1, weight: 1 },
+        //     { name: "treat_762", count: 1, weight: 1 },
+        //     { name: "treat_super", count: 1, weight: 0.1 },
+        // ],
+    },
     mapGen: {
         densitySpawns: [
             {
